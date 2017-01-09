34d3634d8da103a58ec318801f1c73cd832b9d9d
Jenny <jenny.den.ouden@gmail.com>
2017-01-09 09:45:26 +0100

    Project aangemaakt via spring.io

---
+++ src/main/java/home/jy/WormsApplication.java
@@ -0,0 +1,12 @@
+package home.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WormsApplication.class, args);
+	}
+}
